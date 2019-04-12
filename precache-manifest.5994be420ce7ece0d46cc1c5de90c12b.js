c94f72e0075c834fef34f1ed118fa5aa0003c376
Dung Kim <dungnk12@gmail.com>
2019-04-12 15:49:19 +0700

    Updates

---
+++ precache-manifest.5994be420ce7ece0d46cc1c5de90c12b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffa45953f77e2d3df2d0",
+    "url": "/detectcmnd1/static/css/main.9ab4979a.chunk.css"
+  },
+  {
+    "revision": "ffa45953f77e2d3df2d0",
+    "url": "/detectcmnd1/static/js/main.735c3078.chunk.js"
+  },
+  {
+    "revision": "fb369c39497495bc88b7",
+    "url": "/detectcmnd1/static/js/runtime~main.9c9e2255.js"
+  },
+  {
+    "revision": "f936883e69013787661f",
+    "url": "/detectcmnd1/static/js/2.d1638815.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/detectcmnd1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2ea311feecac128447a4a862a1be7322",
+    "url": "/detectcmnd1/index.html"
+  }
+];
